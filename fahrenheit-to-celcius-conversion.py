00d1196bc4547adb78347b077021cdc48ddabf57
Elizabeth Wooton <elizabeth@greatscottgadgets.com>
2019-09-30 12:39:37 -0600

    Simplified and tweaked code

--- fahrenheit-to-celcius-conversion.py
+++ fahrenheit-to-celcius-conversion.py
@@ -1,18 +1,15 @@
-#assigns user input, which answers the prompt "Enter Fahrenheit Temperature:"), with the variable 'inp'
-inp = input('Enter Fahrenheit Temperature: ')
 try:
-	#this next line converts the string (which is how the user input 	is interpreted by python) to a floating point integer.
-	fahr = float(inp)
-	#now, the conversion math...
-	cel = (fahr - 32.0) * 5.0 / 9.0
-	#user sees:
-	print(cel)
-#but what happens if the user enters something that isn't a number?
+	fahrenheit_temp = input('Enter Fahrenheit Temperature: ')
+	f = float(fahrenheit_temp)
+	c = (f - 32.0) * 5.0 / 9.0	
+	print('Celsius = ', c)
 except:
-	print ('Please enter an number')
-	inp = input('Enter Fahrenheit Temperature:')
-	fahr = float(inp)
-	#now, the conversion math...
-	cel = (fahr - 32.0) * 5.0 / 9.0
-	#user sees:
-	print(cel)
+	print ('Please enter an integer')
+try:
+	fahrenheit_temp = input('Enter Fahrenheit Temperature: ')
+	f = float(fahrenheit_temp)
+	c = (f - 32.0) * 5.0 / 9.0	
+	print('Celsius = ', c)
+except:
+	print ('That was not an integer. Start over.')
+
